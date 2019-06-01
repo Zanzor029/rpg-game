5d4591a4c3e9e17bba38bba612d9e9f7742edbd4
Zanzor029 <zanzor029@gmail.com>
2019-06-01 15:43:04 +0200

    Added Font

--- src/components/characterlist.js
+++ src/components/characterlist.js
@@ -3,77 +3,77 @@ import '../App.css';
 import CharacterListButtons from './characterlistbuttons';
 
 class CharacterList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      characters: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            characters: []
+        };
+    }
 
-  componentDidMount() {
-    this.getCharacterData()
-  }
+    componentDidMount() {
+        this.getCharacterData()
+    }
 
-  getCharacterData() {
-    fetch("http://rpgapi.jpersson.eu:3003/characters")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            characters: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+    getCharacterData() {
+        fetch("http://rpgapi.jpersson.eu:3003/characters")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        characters: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, characters } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
         }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, characters } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
-    else if (!isLoaded) {
-      return <div>Loading...</div>;
-    }
-    else {
-      return (
-        <div id="CharacterListPanel">
-          <div id="CharacterListUlHolder">
-            <ul id="CharacterList">
-              {characters.map(character => (
-                <li className="CharacterList">
-                    <div className="CharacterListRow">
-                  <div className="CharacterListName">
-                    {character.Name}
-                  </div>
-                  <div className="CharacterListImagesHolder">
-                    <div className="CharacterRaceImageHolder">
-                      <img className="CharacterRaceImage" src={character.RaceIconPath} />
+        else if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+        else {
+            return (
+                <div id="CharacterListPanel">
+                    <div id="CharacterListUlHolder">
+                        <ul id="CharacterList">
+                            {characters.map(character => (
+                                <li className="CharacterList" id={"CharacterListLiID-"+character.Id}>
+                                    <div className="CharacterListRow" id={"CharacterListRowID-"+character.Id}>
+                                        <div className="CharacterListName">
+                                            {character.Name}
+                                        </div>
+                                        <div className="CharacterListImagesHolder">
+                                            <div className="CharacterRaceImageHolder">
+                                                <img className="CharacterRaceImage" src={character.RaceIconPath} />
+                                            </div>
+                                            <div className="CharacterClassImageHolder">
+                                                <img className="CharacterClassImage" src={character.ClassIconPath} />
+                                            </div>
+                                        </div>
+                                        <div className="CharacterListLevelRaceClass">
+                                            Level {character.Level} {character.Race} {character.Class}
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                    <div className="CharacterClassImageHolder">
-                      <img className="CharacterClassImage" src={character.ClassIconPath} />
-                    </div>
-                  </div>
-                  <div className="CharacterListLevelRaceClass">
-                    Level {character.Level} {character.Race} {character.Class}
-                  </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <CharacterListButtons/>
-        </div>
-      );
+                    <CharacterListButtons />
+                </div>
+            );
+        }
     }
-  }
 }
 export default CharacterList;
